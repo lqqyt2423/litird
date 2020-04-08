449a457f2040d9d1ebb96af81ad16462e038a2da
liqiang <gymbo@gymbodeMacBook-Pro.local>
2020-04-08 18:43:36 +0800

    upgrade d.ts

--- index.d.ts
+++ index.d.ts
@@ -8,47 +8,8 @@ import ExportPaginate = require('./mongoose/plugin/paginate');
 import Router = require('koa-router');
 import Koa = require('koa');
 
-declare namespace Litird {
-  type Config = typeof ExportConfig;
-  interface IConfig extends Config { };
-  interface IMongoosePlugins {
-    motime: typeof ExportMotime;
-    paginate: typeof ExportPaginate;
-  }
-
-  interface IModel { }
-
-  interface IService { }
-
-  interface IController { }
-
-  export class Service {
-    app: Litird;
-
-    config: Litird.IConfig;
-    logger: EggConsoleLogger;
-    redis: IORedis.Redis;
-    model: Litird.IModel;
-    service: Litird.IService;
-  }
-
-  export class Controller {
-    app: Litird;
-
-    config: Litird.IConfig;
-    logger: EggConsoleLogger;
-    redis: IORedis.Redis;
-    model: Litird.IModel;
-    entity: any;
-    service: Litird.IService;
-    server: Koa;
-    validator: any;
-    validate: any;
-  }
-}
-
 declare class Litird {
-  static app: Litird;
+  app: Litird.App;
 
   config: Litird.IConfig;
   logger: EggConsoleLogger;
@@ -70,6 +31,48 @@ declare class Litird {
   beforeMountRouter(): void;
   onstart(): void;
   onready(): void;
+}
+
+declare namespace Litird {
+  type Config = typeof ExportConfig;
+  interface IConfig extends Config { };
+
+  interface IMongoosePlugins {
+    motime: typeof ExportMotime;
+    paginate: typeof ExportPaginate;
+  }
+
+  interface IModel { }
+
+  interface IService { }
+
+  interface IController { }
+
+  interface App extends Litird { }
+
+  export class Service {
+    app: App;
+
+    config: IConfig;
+    logger: EggConsoleLogger;
+    redis: IORedis.Redis;
+    model: IModel;
+    service: IService;
+  }
+
+  export class Controller {
+    app: App;
+
+    config: IConfig;
+    logger: EggConsoleLogger;
+    redis: IORedis.Redis;
+    model: IModel;
+    entity: any;
+    service: IService;
+    server: Koa;
+    validator: any;
+    validate: any;
+  }
 }
 
 export = Litird;
